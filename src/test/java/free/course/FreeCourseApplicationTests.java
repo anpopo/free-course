3d53e8f14f5ad5829f1cb5be8663c7ce91c90c4b
anpopo <dkstpgud@gmail.com>
2021-11-09 08:33:00 +0900

    initial commit

---
+++ src/test/java/free/course/FreeCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package free.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreeCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
