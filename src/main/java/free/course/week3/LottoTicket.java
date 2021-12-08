4d1078c7bf9db9c44adf57eee56e4dfb15079dc0
anpopo <dkstpgud@gmail.com>
2021-12-08 20:59:13 +0900

    중복 되지 않은 번호를 6개 가진 로또 티켓 생성

--- src/main/java/free/course/week3/LottoTicket.java
+++ src/main/java/free/course/week3/LottoTicket.java
@@ -1,6 +1,26 @@
 package free.course.week3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoTicket {
 
-    public LottoTicket() {}
+    private final List<LottoNumber> lottoNumbers;
+
+    public LottoTicket() {
+        lottoNumbers = new ArrayList<>();
+
+        while(lottoNumbers.size() < 6) {
+            LottoNumber lottoNumber = LottoNumberGenerator.generateLottoNumber();
+            lottoNumberDuplicateChecker(lottoNumber);
+        }
+    }
+
+    private void lottoNumberDuplicateChecker(LottoNumber lottoNumber) {
+        if(!lottoNumbers.contains(lottoNumber)) lottoNumbers.add(lottoNumber);
+    }
+
+    public List<LottoNumber> getLottoNumbers() {
+        return new ArrayList<>(lottoNumbers);
+    }
 }
--- src/test/java/free/course/week3/LottoTicketTest.java
+++ src/test/java/free/course/week3/LottoTicketTest.java
@@ -1,7 +1,9 @@
 package free.course.week3;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 public class LottoTicketTest {
@@ -13,5 +15,25 @@ public class LottoTicketTest {
         LottoTicket lottoTicket = LottoTicketSeller.createLottoTicket(0);
 
         Assertions.assertThat(lottoTicket).isNotNull();
+    }
+
+    @Disabled
+    @RepeatedTest(200)
+    @DisplayName("로또 티켓 생성 - 돈을 안내도 로또 티켓을 생성 해 준다. - 중복될 수도 있는 번호 6개를 가진")
+    void createLottoTicketWithLottoNumbersNoRandom() {
+        LottoTicket lottoTicket = LottoTicketSeller.createLottoTicket(0);
+
+        Assertions.assertThat(lottoTicket.getLottoNumbers().size()).isEqualTo(6);
+        Assertions.assertThat(lottoTicket.getLottoNumbers()).doesNotHaveDuplicates();
+    }
+
+    @RepeatedTest(200)
+    @DisplayName("로또 티켓 생성 - 돈을 안내도 로또 티켓을 생성 해 준다. - 중복 없는 번호 6개를 가진")
+    void createLottoTicketWithLottoNumbersYesRandom() {
+        LottoTicket lottoTicket = LottoTicketSeller.createLottoTicket(0);
+
+        Assertions.assertThat(lottoTicket.getLottoNumbers().size()).isEqualTo(6);
+        Assertions.assertThat(lottoTicket.getLottoNumbers()).doesNotHaveDuplicates();
+
     }
 }
